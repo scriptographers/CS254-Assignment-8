fda07972d571a122fb5e31130beb1ed6767af79c
devansh-dvj <devansh.dvj@gmail.com>
2021-04-10 19:27:46 +0530

    Integrated State Machine using structural code in TrafficLightController.vhd

--- Q1/TrafficLightController.vhd
+++ Q1/TrafficLightController.vhd
@@ -4,7 +4,6 @@ use work.all;
 
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all; -- for the signed, unsigned types and arithmetic ops
 
 entity TrafficLightController is
 	port (
@@ -15,81 +14,28 @@ end entity;
 
 architecture struct of TrafficLightController is
 
-	signal s : integer := 0;
-	signal i : integer := 1;
+	signal lane_clk, g : std_logic;
+
+	component SM1 is
+		port (
+			clk, rst        : in std_logic;
+			lane_clk, green : out std_logic
+		);
+	end component;
+
+	component SM2 is
+		port (
+			clk, rst           : in std_logic;
+			green, yellow, red : out std_logic_vector(3 downto 0)
+		);
+	end component;
 
 begin
-	update_output : process (s)
-	begin
 
-		green <= "0000";
-		red <= "1111";
-		yellow <= "0000";
+	Machine1 : SM1
+	port map(clk => clk, rst => rst, lane_clk => lane_clk, green => g);
 
-		case s is
-
-				-- Lane 0
-			when 0 =>
-				green(0) <= '1';
-				red(0) <= '0';
-
-			when 1 =>
-				yellow(0) <= '1';
-				red(0) <= '0';
-
-				-- Lane 1
-			when 2 =>
-				green(1) <= '1';
-				red(1) <= '0';
-
-			when 3 =>
-				yellow(1) <= '1';
-				red(1) <= '0';
-
-				-- Lane 2
-			when 4 =>
-				green(2) <= '1';
-				red(2) <= '0';
-
-			when 5 =>
-				yellow(2) <= '1';
-				red(2) <= '0';
-				-- Lane 3
-			when 6 =>
-				green(3) <= '1';
-				red(3) <= '0';
-
-			when 7 =>
-				yellow(3) <= '1';
-				red(3) <= '0';
-
-			when others =>
-				green <= "0000";
-				red <= "1111";
-				yellow <= "0000";
-
-		end case;
-
-	end process;
-
-	main_process : process (clk)
-	begin
-		if (rising_edge(clk)) then
-
-			case i is
-
-				when 30 =>
-					s <= (s + 1) mod 8;
-					i <= i + 1;
-				when 35 =>
-					s <= (s + 1) mod 8;
-					i <= 1; -- reset after 35
-				when others =>
-					i <= i + 1;
-
-			end case;
-
-		end if;
-	end process;
+	Machine2 : SM2
+	port map(clk => lane_clk, rst => rst, green => green, yellow => yellow, red => red);
 
 end architecture;
